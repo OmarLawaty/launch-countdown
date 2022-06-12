6b281610a5ab312ce2f16d879f10232b56877052
Omar Mohamed <omarlawatey@gmail.com>
2022-06-12 08:01:08 +0200

    feat: Implement flipping cards and some code improvement

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
-const date = new Date('May 30, 2023 2:17:00').getTime();
+const date = new Date('june 30, 2023 2:24:00').getTime();
 
 const timeTypes = [
   {
     type: 'Days',
     value: '0',
     element: new Object(),
-    update: time => Math.floor(time / (1000 * 60 * 60 * 24))
+    updateTo: time => Math.floor(time / (1000 * 60 * 60 * 24)),
+    updateFrom: time => time * (1000 * 60 * 60 * 24)
   },
   {
     type: 'Hours',
     value: '0',
     element: new Object(),
-    update: time => Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    updateTo: time => Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    updateFrom: time => time * 3600000
   },
   {
     type: 'Minutes',
     value: '0',
     element: new Object(),
-    update: time => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    updateTo: time => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+    updateFrom: time => time * 60000
   },
   {
     type: 'Seconds',
     value: '0',
     element: new Object(),
-    update: time => Math.floor((time % (1000 * 60)) / 1000)
+    updateTo: time => Math.floor((time % (1000 * 60)) / 1000),
+    updateFrom: time => time * 1000
   }
 ];
 
@@ -33,23 +37,62 @@ const fragment = document.createDocumentFragment();
 timeTypes.forEach(({ type, value }, index) => {
   let timerItems = document.createElement('div');
   timerItems.classList = `timer-items ${type.toLowerCase()}`;
-  timerItems.innerHTML = `<span class="time-value">${value}</span> <span class="time-type">${type}</span>`;
+  timerItems.innerHTML = `
+  <div class="time-value">
+    <div class="next-number">${value}</div>
+    <div class="current-number">${value}</div>
+      <div class="new-values">
+        <div class="upper-value" >${value}</div>
+        <div class="bottom-value" >${value}</div>
+      </div>
+  </div>
+  <div class="time-type">${type}</div>`;
 
   timeTypes[index].element = timerItems;
+
   fragment.appendChild(timerItems);
 });
+
 countdownContainer.appendChild(fragment);
 
 const updateTimer = setInterval(() => {
   const now = new Date().getTime();
   let timeLeft = date - now;
 
-  timeTypes.forEach(({ element, update }) => appendTextToElement(element.children[0], update(timeLeft)));
+  timeTypes.forEach(({ element, updateTo, updateFrom }) => {
+    element = element.children[0].children[1];
+
+    if (checkSameState(Number(element.innerHTML), updateTo(timeLeft))) return;
+
+    flip(element);
+
+    setTimeout(() => appendTextToElement(element, updateTo(timeLeft), updateTo(timeLeft - updateFrom(1))), 500);
+  });
 
   if (timeLeft <= 0) {
     clearInterval(updateTimer);
-    timeTypes.forEach(({ element, value }) => appendTextToElement(element.children[0], value));
+    timeTypes.forEach(({ element, value }) => appendTextToElement(element.children[0], value, value));
   }
 }, 1000);
 
-const appendTextToElement = (element, text) => (element.innerHTML = text);
+const appendTextToElement = (element, text, nextValue) => {
+  element.parentElement.children[0].innerHTML = nextValue;
+  element.parentElement.children[2].children[1].innerHTML = nextValue;
+  element.parentElement.children[2].children[0].innerHTML = text;
+  element.parentElement.children[1].innerHTML = text;
+};
+
+const flip = element => {
+  element.parentElement.classList.add('flip');
+  setTimeout(() => {
+    element.parentElement.classList.remove('flip');
+  }, 500);
+};
+
+const checkSameState = (currentValue, nextValue) => {
+  if (currentValue === nextValue) {
+    return true;
+  }
+
+  return false;
+};
